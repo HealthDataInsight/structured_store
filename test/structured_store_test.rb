1b3c01af5c0d63c69c14fe2e007a032b10b9ae54
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-05-30 17:38:22 +0100

    Improved version test

--- test/structured_store_test.rb
+++ test/structured_store_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class StructuredStoreTest < ActiveSupport::TestCase
   test "it has a version number" do
-    assert StructuredStore::VERSION
+    assert_kind_of String, StructuredStore::VERSION
+    assert_match Gem::Version::ANCHORED_VERSION_PATTERN, StructuredStore::VERSION
   end
 end
