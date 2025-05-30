a5850ebbde643226abe947dd13e2d9ea42345894
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-05-30 10:22:31 +0100

    Added the versioned schema

---
+++ app/models/structured_store/versioned_schema.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+require 'json'
+
+module StructuredStore
+  # This model stores individual versions of each structured store schema
+  class VersionedSchema < ApplicationRecord
+    # TODO: More JSON schema validations:
+    # - none of the properties is an object (preserving a flat structure)
+    # - additionalProperties must be false
+    # - check all required properties are defined in properties
+    validates :json_schema, json_schema: { allow_blank: true, schema: :draft201909 }
+    validates :name, presence: true, uniqueness: { scope: :version, case_sensitive: true }
+    validates :version, presence: true, format: { with: Gem::Version::ANCHORED_VERSION_PATTERN }
+
+    store_accessor :json_schema, :definitions, :properties
+
+    def self.latest(name)
+      schemas = where(name: name)
+
+      # Return nil if no schemas with this name exist
+      return nil if schemas.empty?
+
+      # Sort by version using gem_version and return the last one (highest version)
+      schemas.max_by(&:gem_version)
+    end
+
+    def json_schema=(json)
+      case json
+      when String
+        super(JSON.parse(json))
+      else
+        super
+      end
+    end
+
+    def formatted_json_schema
+      return nil if json_schema.nil?
+
+      JSON.pretty_generate(json_schema)
+    end
+
+    def gem_version
+      Gem::Version.new(version)
+    end
+
+    # Returns the lookup options for a given lookup.
+    def lookup_options(lookup)
+      definition = definitions[lookup.to_s]
+
+      raise "No definition for #{lookup}" if definition.nil?
+
+      definition['enum'] || []
+    end
+
+    # TODO: Do we still need this?
+    def field_options(field)
+      definition_name = local_definition_name(field)
+
+      lookup_options(definition_name)
+    end
+
+    # Returns the full property definition for a given field.
+    # This includes the local definition if the property references one.
+    def full_property_definition(field)
+      hash = properties[field.to_s]
+      definition_name = local_definition_name(field)
+      local_definition = definitions[definition_name]
+
+      hash = hash.merge(local_definition) unless local_definition.nil?
+      hash
+    end
+
+    # Returns the local definition name for a given field.
+    # It raises an error if the field has a reference that is not local.
+    def local_definition_name(field)
+      hash = properties[field.to_s]
+
+      raise "No property definition for #{field}" if hash.nil?
+
+      ref = hash['$ref']
+
+      return nil if ref.nil?
+      raise "Not a local reference in $ref: #{ref}" unless ref.start_with?('#/definitions/')
+
+      ref.sub('#/definitions/', '')
+    end
+  end
+end
---
+++ test/models/structured_store/versioned_schema_test.rb
@@ -0,0 +1,112 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module StructuredStore
+  # This class contains the StructuredStore::VersionedSchema unit tests
+  class VersionedSchemaTest < ActiveSupport::TestCase
+    test 'fixture' do
+      versioned_schema = StructuredStore::VersionedSchema.latest('MyAudit')
+
+      assert_kind_of StructuredStore::VersionedSchema, versioned_schema
+      assert_kind_of Hash, versioned_schema.json_schema
+      assert_equal 'http://json-schema.org/draft/2019-09/schema#', versioned_schema.json_schema['$schema']
+    end
+
+    test 'json_schema validation' do
+      versioned_schema = StructuredStore::VersionedSchema.new
+      versioned_schema.valid?
+
+      assert_empty versioned_schema.errors.details[:json_schema]
+
+      versioned_schema.json_schema = JSON.dump(1)
+      versioned_schema.valid?
+
+      assert_includes versioned_schema.errors.details[:json_schema],
+                      error: 'value at root is not one of the types: ["object", "boolean"]'
+
+      versioned_schema.json_schema = <<~STR
+        {
+          "$schema": "http://json-schema.org/draft/2019-09/schema#",
+          "type": "object",
+          "properties": {
+            "theme": {
+              "type": "string",
+              "description": "User interface theme preference"
+            }
+          },
+          "required": ["theme"],
+          "additionalProperties": false
+        }
+      STR
+      versioned_schema.valid?
+
+      assert_empty versioned_schema.errors.details[:json_schema]
+    end
+
+    test 'name validation' do
+      versioned_schema = StructuredStore::VersionedSchema.new
+      versioned_schema.valid?
+
+      assert_includes versioned_schema.errors.details[:name], error: :blank
+
+      versioned_schema.name = 'Potato'
+      versioned_schema.valid?
+
+      assert_not_includes versioned_schema.errors.details[:name], error: :blank
+    end
+
+    test 'field_options' do
+      versioned_schema = StructuredStore::VersionedSchema.new
+
+      versioned_schema.json_schema = <<~STR
+        {
+          "$schema": "http://json-schema.org/draft/2019-09/schema#",
+          "type": "object",
+          "definitions": {
+            "zyes_no": {
+              "type": "string",
+              "enum": [
+                "Yes",
+                "No"
+              ],
+              "description": "CCA No lookup"
+            }
+          },
+          "properties": {
+            "select_field_via_ref": {
+              "$ref": "#/definitions/zyes_no",
+              "description": "Evidence in patient’s records that patient was signposted to ..."
+            },
+            "select_field": {
+              "type": "string",
+              "enum": ["option1", "option2", "option3"]
+            }
+          },
+          "additionalProperties": false
+        }
+      STR
+
+      assert_equal %w[Yes No], versioned_schema.field_options(:select_field_via_ref)
+      assert_equal %w[option1 option2 option3], versioned_schema.field_options(:select_field)
+    end
+
+    test 'version validation' do
+      versioned_schema = StructuredStore::VersionedSchema.new
+      versioned_schema.valid?
+
+      assert_includes versioned_schema.errors.details[:version], error: :blank
+
+      versioned_schema.version = 'Potato'
+      versioned_schema.valid?
+
+      assert_not_includes versioned_schema.errors.details[:version], error: :blank
+      assert_includes versioned_schema.errors.details[:version], { error: :invalid, value: 'Potato' }
+
+      versioned_schema.version = '0.10.0'
+      versioned_schema.valid?
+
+      assert_not_includes versioned_schema.errors.details[:version], { error: :invalid, value: '0.10.0' }
+    end
+  end
+end
