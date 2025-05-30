3d03781dfa199183e66536f507c1f54db90570e3
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-05-30 11:25:06 +0100

    Added engine file and zeitwerk dependency

--- lib/structured_store.rb
+++ lib/structured_store.rb
@@ -1,6 +1,12 @@
 require "structured_store/version"
 require "structured_store/railtie"
 
+require 'zeitwerk'
+loader = Zeitwerk::Loader.for_gem
+loader.setup
+
+require 'structured_store/engine'
+
 module StructuredStore
   # Your code goes here...
 end
