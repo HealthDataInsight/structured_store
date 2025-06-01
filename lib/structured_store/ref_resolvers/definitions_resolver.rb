7cb74382a3a5978ae41759c46d88c8e521d209d8
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-06-01 21:36:42 +0100

    DefinitionsResolver

---
+++ lib/structured_store/ref_resolvers/definitions_resolver.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'structured_store/ref_resolvers/base'
+require 'structured_store/ref_resolvers/registry'
+
+module StructuredStore
+  # This is the namespace for all reference resolvers used in StructuredStore.
+  module RefResolvers
+    # This class resolves $ref strings that point to definitions within the schema.
+    class DefinitionsResolver < Base
+      def initialize(schema, property_name, ref_string, context = {})
+        super
+      end
+
+      # Defines the rails attribute(s) on the given singleton class
+      #
+      # @return [Proc] a lambda that defines the attribute on the singleton class
+      # @raise [RuntimeError] if the property type is unsupported
+      def define_attribute
+        type = local_definition['type']
+
+        unless %w[boolean integer string].include?(type)
+          raise "Unsupported attribute type: #{type.inspect} for property '#{property_name}'"
+        end
+
+        # Define the attribute on the singleton class of the object
+        lambda do |object|
+          object.singleton_class.attribute(property_name, type.to_sym)
+        end
+      end
+
+      private
+
+      # Retrieves a local definition from the schema based on the reference string
+      #
+      # @return [Hash] The local definition hash from the schema's definitions
+      # @raise [RuntimeError] If no definition is found for the given reference string
+      # @example
+      #   resolver.local_definition  # => { "type" => "string" }
+      def local_definition
+        definition_name = ref_string.sub('#/definitions/', '')
+        local_definition = schema.dig('definitions', definition_name) || {}
+
+        raise "No definition for #{ref_string}" if local_definition.nil?
+
+        local_definition
+      end
+    end
+
+    # Register the DefinitionsResolver with a specific pattern
+    Registry.register(DefinitionsResolver, %r{^#/definitions/})
+  end
+end
---
+++ test/dummy/test/ref_resolvers/definitions_resolver_test.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+require 'structured_store/ref_resolvers/registry'
+require 'test_helper'
+
+module StructuredStore
+  module RefResolvers
+    # This class tests the DefinitionsResolver.
+    class DefinitionsResolverTest < ActiveSupport::TestCase
+      test 'matching_resolver' do
+        schema = {
+          '$schema' => 'http://json-schema.org/draft/2019-09/schema#',
+          'type' => 'object',
+          'definitions' => {
+            'foo_lookup' => {
+              'type' => 'string',
+              'description' => 'A foo property'
+            }
+          },
+          'properties' => {
+            'foo' => {
+              '$ref': '#/definitions/foo_lookup'
+            }
+          }
+        }
+
+        resolver = Registry.matching_resolver(schema, 'foo', '#/definitions/foo_lookup')
+        assert_instance_of DefinitionsResolver, resolver
+      end
+
+      test 'define_attribute with string attribute' do
+        schema = {
+          '$schema' => 'http://json-schema.org/draft/2019-09/schema#',
+          'type' => 'object',
+          'definitions' => {
+            'foo_lookup' => {
+              'type' => 'string',
+              'description' => 'A foo property'
+            }
+          },
+          'properties' => {
+            'foo' => {
+              '$ref': '#/definitions/foo_lookup'
+            }
+          }
+        }
+
+        store_record = StoreRecord.new(versioned_schema: VersionedSchema.new(json_schema: schema))
+
+        # Now the structured store attribute "foo" should be defined
+        assert_nil store_record.foo
+        store_record.foo = 'bar'
+        assert_equal 'bar', store_record.foo
+      end
+
+      test 'define_attribute with untested attribute type' do
+        schema = {
+          '$schema' => 'http://json-schema.org/draft/2019-09/schema#',
+          type: 'object',
+          definitions: {
+            'foo_lookup' => {
+              'type' => 'object'
+            }
+          },
+          'properties' => {
+            'foo' => {
+              '$ref': '#/definitions/foo_lookup'
+            }
+          }
+        }
+
+        exception = assert_raises(RuntimeError) do
+          StoreRecord.new(versioned_schema: VersionedSchema.new(json_schema: schema))
+        end
+
+        assert_equal 'Unsupported attribute type: "object" for property \'foo\'', exception.message
+      end
+    end
+  end
+end
