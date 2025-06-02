86cbf61269e966fa300feab001b7e2b5d645ce8d
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-06-02 07:19:26 +0100

    Linting

--- lib/structured_store.rb
+++ lib/structured_store.rb
@@ -1,4 +1,4 @@
-require "structured_store/version"
+require 'structured_store/version'
 
 require 'zeitwerk'
 loader = Zeitwerk::Loader.for_gem
@@ -8,6 +8,7 @@ require 'structured_store/engine'
 require 'structured_store/generators/install_generator' if defined?(Rails::Generators)
 require 'structured_store/ref_resolvers/registry'
 
+# This module serves as a namespace for the StructuredStore gem
 module StructuredStore
   # Your code goes here...
 end
