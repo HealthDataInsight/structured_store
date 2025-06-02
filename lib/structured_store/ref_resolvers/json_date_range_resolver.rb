6e72ae55be9370ff54518363c69c72d16f43e293
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-06-02 16:26:44 +0100

    JsonDateRangeResolver

---
+++ lib/structured_store/ref_resolvers/json_date_range_resolver.rb
@@ -0,0 +1,32 @@
+# json_date_range
+# frozen_string_literal: true
+
+require 'structured_store/ref_resolvers/base'
+require 'structured_store/types/json_date_range_type'
+
+module StructuredStore
+  # This is the namespace for all reference resolvers used in StructuredStore.
+  module RefResolvers
+    # This class resolves properties where no $ref is defined.
+    class JsonDateRangeResolver < Base
+      def self.matching_ref_pattern
+        %r{\Aexternal://json_date_range/}
+      end
+
+      # Defines the rails attribute(s) on the given singleton class
+      #
+      # @return [Proc] a lambda that defines the attribute on the singleton class
+      # @raise [RuntimeError] if the property type is unsupported
+      def define_attribute
+        # Define the attribute on the singleton class of the object
+        lambda do |object|
+          object.singleton_class.attribute(property_name, :json_date_range,
+                                           date_range_converter: object.date_range_converter)
+        end
+      end
+    end
+
+    # Register the JsonDateRangeResolver with the registry
+    JsonDateRangeResolver.register
+  end
+end
---
+++ test/dummy/test/ref_resolvers/json_date_range_resolver_test.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'structured_store/ref_resolvers/json_date_range_resolver'
+require 'test_helper'
+
+module StructuredStore
+  module RefResolvers
+    # This class tests the JsonDateRangeResolver.
+    class JsonDateRangeResolverTest < ActiveSupport::TestCase
+      test 'matching_resolver' do
+        schema = {
+          '$schema' => 'http://json-schema.org/draft/2019-09/schema#',
+          'type' => 'object',
+          'properties' => {
+            'foo' => {
+              '$ref': 'external://json_date_range/'
+            }
+          }
+        }
+
+        resolver = Registry.matching_resolver(schema, 'foo')
+        assert_instance_of JsonDateRangeResolver, resolver
+      end
+
+      test 'define_attribute with string attribute' do
+        schema = {
+          '$schema' => 'http://json-schema.org/draft/2019-09/schema#',
+          'type' => 'object',
+          'properties' => {
+            'foo' => {
+              '$ref': 'external://json_date_range/'
+            }
+          }
+        }
+
+        store_record = StoreRecord.new(versioned_schema: VersionedSchema.new(json_schema: schema))
+
+        # Now the structured store attribute "foo" should be defined
+        assert_nil store_record.foo
+        store_record.foo = { 'date1' => '2024-01-01', 'date2' => '2024-01-31' }
+        assert_equal({ 'date1' => '2024-01-01', 'date2' => '2024-01-31' }, store_record.foo)
+      end
+    end
+  end
+end
