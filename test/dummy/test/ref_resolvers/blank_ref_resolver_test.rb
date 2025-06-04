67986163573fa27153ea34485c7a553f99da1cbe
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-06-04 13:43:25 +0100

    Moved tests that can be moved

--- test/dummy/test/ref_resolvers/blank_ref_resolver_test.rb
+++ test/dummy/test/ref_resolvers/blank_ref_resolver_test.rb
@@ -5,32 +5,8 @@ require 'test_helper'
 
 module StructuredStore
   module RefResolvers
-    # This class tests the BlankRefResolver.
+    # This class tests the BlankRefResolver within a structured store record.
     class BlankRefResolverTest < ActiveSupport::TestCase
-      test 'matching_ref_pattern' do
-        assert_match BlankRefResolver.matching_ref_pattern, ''
-
-        assert_no_match BlankRefResolver.matching_ref_pattern, '#/definitions/foo'
-        assert_no_match BlankRefResolver.matching_ref_pattern, 'external://custom_lookup/yes_no_unknown'
-        assert_no_match BlankRefResolver.matching_ref_pattern, 'external://structured_store/json_date_range/'
-      end
-
-      test 'matching_resolver' do
-        schema = {
-          '$schema' => 'http://json-schema.org/draft/2019-09/schema#',
-          'type' => 'object',
-          'properties' => {
-            'foo' => {
-              'type' => 'string',
-              'description' => 'A foo property'
-            }
-          }
-        }
-
-        resolver = Registry.matching_resolver(schema, 'foo')
-        assert_instance_of BlankRefResolver, resolver
-      end
-
       test 'define_attribute with no attribute' do
         schema = {
           '$schema' => 'http://json-schema.org/draft/2019-09/schema#',
@@ -81,25 +57,6 @@ module StructuredStore
         end
 
         assert_equal 'Unsupported attribute type: "object" for property \'foo\'', exception.message
-      end
-
-      test 'options_array' do
-        schema = {
-          '$schema' => 'http://json-schema.org/draft/2019-09/schema#',
-          'type' => 'object',
-          'properties' => {
-            'foo' => {
-              'type' => 'string',
-              'enum' => %w[option1 option2 option3]
-            }
-          },
-          'additionalProperties' => false
-        }
-
-        resolver = Registry.matching_resolver(schema, 'foo')
-        assert_instance_of BlankRefResolver, resolver
-
-        assert_equal [%w[option1 option1], %w[option2 option2], %w[option3 option3]], resolver.options_array
       end
     end
   end
