2b40ea76ccdead597b5d28391ea05aa5f4b88fff
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-07-15 17:40:20 +0100

    Partial commit of more tests

--- test/dummy/test/models/store_record_test.rb
+++ test/dummy/test/models/store_record_test.rb
@@ -42,42 +42,42 @@ class StoreRecordTest < ActiveSupport::TestCase
   #   assert_equal expected_store, model.store
   # end
 
-  # test 'works with different schema versions' do
-  #   # Create version 1 schema
-  #   schema_v1 = StructuredStore::VersionedSchema.create!(
-  #     name: 'test_schema',
-  #     version: '1.0.0',
-  #     json_schema: {
-  #       'type' => 'object',
-  #       'properties' => {
-  #         'name' => { 'type' => 'string' }
-  #       }
-  #     }
-  #   )
+  test 'works with different schema versions' do
+    # Create version 1 schema
+    schema_v1 = StructuredStore::VersionedSchema.create!(
+      name: 'test_schema',
+      version: '1.0.0',
+      json_schema: {
+        'type' => 'object',
+        'properties' => {
+          'name' => { 'type' => 'string' }
+        }
+      }
+    )
 
-  #   # Create version 2 schema with additional field
-  #   schema_v2 = StructuredStore::VersionedSchema.create!(
-  #     name: 'test_schema',
-  #     version: '2.0.0',
-  #     json_schema: {
-  #       'type' => 'object',
-  #       'properties' => {
-  #         'name' => { 'type' => 'string' },
-  #         'description' => { 'type' => 'string' }
-  #       }
-  #     }
-  #   )
+    # Create version 2 schema with additional field
+    schema_v2 = StructuredStore::VersionedSchema.create!(
+      name: 'test_schema',
+      version: '2.0.0',
+      json_schema: {
+        'type' => 'object',
+        'properties' => {
+          'name' => { 'type' => 'string' },
+          'description' => { 'type' => 'string' }
+        }
+      }
+    )
 
-  #   # Test with v1 schema
-  #   model_v1 = ExplicitStoreRecord.new(store_versioned_schema: schema_v1)
-  #   assert model_v1.respond_to?(:name)
-  #   assert_not model_v1.respond_to?(:description)
+    # Test with v1 schema
+    record_v1 = StoreRecord.new(store_versioned_schema: schema_v1)
+    assert record_v1.respond_to?(:name)
+    assert_not record_v1.respond_to?(:description)
 
-  #   # Test with v2 schema
-  #   model_v2 = ExplicitStoreRecord.new(store_versioned_schema: schema_v2)
-  #   assert model_v2.respond_to?(:name)
-  #   assert model_v2.respond_to?(:description)
-  # end
+    # Test with v2 schema
+    record_v2 = StoreRecord.new(store_versioned_schema: schema_v2)
+    assert record_v2.respond_to?(:name)
+    assert record_v2.respond_to?(:description)
+  end
 
   private
 
