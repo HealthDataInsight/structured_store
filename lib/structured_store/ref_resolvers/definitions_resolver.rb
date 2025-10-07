44333486f91f258a7b51a0bf09226da2088c6fa5
Tim Gentry <52189+timgentry@users.noreply.github.com>
2025-10-07 22:19:51 +0100

    Housekeeping

--- lib/structured_store/ref_resolvers/definitions_resolver.rb
+++ lib/structured_store/ref_resolvers/definitions_resolver.rb
@@ -12,10 +12,6 @@ module StructuredStore
         %r{\A#/definitions/}
       end
 
-      def initialize(schema, property_name, ref_string, context = {})
-        super
-      end
-
       # Defines the rails attribute(s) on the given singleton class
       #
       # @return [Proc] a lambda that defines the attribute on the singleton class
